949749c493adc1fc18791d7ffd5a6a5d68a18d7d
Gajanan Dnyandev Shinde <GajananShinde@ATMECS.CORP>
2024-05-14 12:28:38 +0530

    Adding Natours project to git repository

---
+++ controllers/userController.js
@@ -0,0 +1,30 @@
+exports.getAllUsers = (req,res) => {
+    res.status(500).json({
+        status: "error",
+        message : "This route is not yet defined 😁"
+    })
+}
+exports.createUser = (req,res) => {
+    res.status(500).json({
+        status: "error",
+        message : "This route is not yet defined 😁"
+    })
+}
+exports.getUser = (req,res) => {
+    res.status(500).json({
+        status: "error",
+        message : "This route is not yet defined 😁"
+    })
+}
+exports.updateUser = (req,res) => {
+    res.status(500).json({
+        status: "error",
+        message : "This route is not yet defined 😁"
+    })
+}
+exports.deleteUser = (req,res) => {
+    res.status(500).json({
+        status: "error",
+        message : "This route is not yet defined 😁"
+    })
+}
